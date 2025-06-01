2c3c2c2beb5ab819e4fcf5325dcebd069b563d9e
clivedev88m <clive.dev88@gmail.com>
2025-05-31 23:46:57 -0300

    Organização da estrutura correta do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import TodoList from './components/TodoList'
 import tarefas from './data/tarefas.json'
+import TodoInput from './components/TodoInput';
+import Todo from './components/TodoItem/Todo';
+import Filter from './components/Filter';
+import Button from './components/Button/Button';
 
 const App = () => {
 
@@ -13,7 +17,11 @@ const App = () => {
   return (
     <div className='h-full mx-5'>
       <h1 className='text-2xl font-bold mb-4 text-center'>Minhas Tarefas</h1>
+      <TodoInput />;
+      <Todo />
       <TodoList todos={todos} />
+      <Filter />
+      <Button />
     </div>
   )
 }
